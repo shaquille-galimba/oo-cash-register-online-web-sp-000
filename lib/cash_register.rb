672262dfd5d241f0d9964f8ab09265898a386cb0
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-23 21:14:32 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,36 +1,35 @@
 class CashRegister
-  attr_accessor :discount, :total
+  attr_accessor :discount, :total, :items
 
 
   def initialize(discount = nil)
-    @total = 0.to_f
+    @total = 0
     @discount = discount
-    @@items = []
-    @@last_transaction = []
+    @items = []
+    @last_transaction = []
   end
 
   def add_item(item, price, quantity = 1)
+    self.total += price * quantity
     quantity.times do
-      @total += price
-      @@items << item
+      self.items << item
     end
-    @@last_transaction << price * quantity
+    @last_transaction << price * quantity
   end
 
   def apply_discount
-    if @discount == nil
+    if self.discount == nil
       "There is no discount to apply."
     else
-      @total -= @total * (@discount.to_f/100)
-      "After the discount, the total comes to $#{@total.to_i}."
+      self.total -= self.total * (self.discount.to_f/100)
+      "After the discount, the total comes to $#{self.total.to_i}."
     end
   end
 
-  def items
-    @@items
-  end
-
+  #I built this method so that customer could void multiple last transaction
+  #I thought this would be a nice way to remove customer mistakes
+  #Since we don't have a delete transaction method
   def void_last_transaction
-    @total -= @@last_transaction.pop
+    self.total -= @last_transaction.pop
   end
 end
