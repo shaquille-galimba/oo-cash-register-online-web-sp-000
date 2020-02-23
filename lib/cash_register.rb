e699b53904a15e27958c851beb4ff074bd4fc422
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-23 21:01:12 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+class CashRegister
+  attr_accessor :discount, :total
 
+
+  def initialize(discount = nil)
+    @total = 0.to_f
+    @discount = discount
+    @@items = []
+    @@last_transaction = []
+  end
+
+  def add_item(item, price, quantity = 1)
+    quantity.times do
+      @total += price
+      @@items << item
+    end
+    @@last_transaction << price * quantity
+  end
+
+  def apply_discount
+    if @discount == nil
+      "There is no discount to apply."
+    else
+      @total -= @total * (@discount.to_f/100)
+      "After the discount, the total comes to $#{@total.to_i}."
+    end
+  end
+
+  def items
+    @@items
+  end
+
+  def void_last_transaction
+    @total -= @@last_transaction.pop
+  end
+end
